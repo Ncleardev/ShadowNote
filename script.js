00053dbeb1cfb940263f0f92822cb8f0cc4f476c
Nclear <xyz.games.apps@gmail.com>
2022-02-26 00:28:44 +0100

    1.0

---
+++ script.js
@@ -0,0 +1,51 @@
+function Time() {
+  document.getElementById("time").innerHTML = new Date().toLocaleTimeString();
+  setTimeout(Time, 1000);
+}
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  e.returnValue = '1';
+});
+
+function Copy() {
+  navigator.clipboard.writeText(document.getElementById("edittext").value);
+}
+
+function Cut() {
+  navigator.clipboard.writeText(document.getElementById("edittext").value);
+  document.getElementById("edittext").value = "";
+}
+
+function Delete() {
+  document.getElementById("edittext").value = "";
+}
+
+function Undo(){
+  document.execCommand('undo', false, null);
+}
+
+function Redo(){
+  document.execCommand('redo', false, null);
+}
+
+function Translate(){
+  window.open("https://translate.google.com/?text=" + document.getElementById("edittext").value);
+}
+
+function SelectAll(){
+  document.getElementById("edittext").select();
+  document.getElementById("edittext").setSelectionRange(0, 99999);
+}
+
+function Paste(){
+  document.getElementById("edittext").value += navigator.clipboard.readText;
+}
+
+function Save(){
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:attachment/text,' + encodeURI(document.getElementById("edittext").value);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = 'Text.txt';
+  hiddenElement.click();
+}
